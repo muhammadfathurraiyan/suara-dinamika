3769d71b24424df4828c2e3999d8f4994d589ceb
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-12-10 00:15:22 +0700

    update

--- src/components/home/Lingkungan.tsx
+++ src/components/home/Lingkungan.tsx
@@ -9,10 +9,10 @@ export default function Lingkungan() {
         <h1 className="font-semibold text-xl p-2 bg-neutral-950 text-neutral-50 w-fit">
           LINGKUNGAN
         </h1>
-        {/* <div className="h-1 w-full bg-neutral-950" /> */}
+        <div className="h-1 w-full bg-neutral-950 md:hidden" />
       </div>
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-neutral-950/70 to-transparent" />
-      <div className="grid grid-cols-3 max-md:grid-cols-2 gap-2">
+      <div className="grid md:grid-cols-3 max-md:grid-rows-3 gap-2">
         <div className="group h-48 relative overflow-hidden">
           <img
             className="min-w-full max-h-full object-cover group-hover:scale-105 duration-300"
@@ -45,7 +45,7 @@ export default function Lingkungan() {
             </p>
           </div>
         </div>
-        <div className="group h-48 relative overflow-hidden max-md:hidden">
+        <div className="group h-48 relative overflow-hidden">
           <img
             className="min-w-full max-h-full object-cover group-hover:scale-105 duration-300"
             src="https://picsum.photos/600"
