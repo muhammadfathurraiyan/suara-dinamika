a74da2a9fa51349b3dcc76fa8509610dabb97701
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-01-14 20:32:39 +0700

    update

--- src/app/admin/account/page.tsx
+++ src/app/admin/account/page.tsx
@@ -1,8 +1,13 @@
+import readUserProfileAction from "@/actions/global/readUserProfileAction";
 import EditPassword from "@/components/admin/account/EditPassword";
 import EditProfile from "@/components/admin/account/EditProfile";
 import ProfileCard from "@/components/admin/account/ProfileCard";
+import { readUserSession } from "@/libs/action";
 
 export default async function Account() {
+  const { data: userSession } = await readUserSession();
+  const { data } = await readUserProfileAction(userSession.session?.user.id!);
+  const profile = data![0];
   return (
     <div className="px-4 py-12 flex flex-col gap-12">
       <div>
